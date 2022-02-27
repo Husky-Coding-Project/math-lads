66834f5cb8a1d76c2b13f4ef55577009d04e19f3
dgunadharma20 <dgunadharma20@gmail.com>
2022-02-26 18:13:57 -0800

    first commit

---
+++ design.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class design {
+   public static void main(String[] args) {
+      System.out.println("Input Your Username: daven4220");
+      
+      System.out.println("");
+      System.out.println("1 - Start New Game");
+      System.out.println("2 - View Personal Scores");
+      System.out.println("Choice: 1\n");
+      
+      System.out.println("Grade 1: Addition & Subtraction");
+      System.out.println("Grade 2: Multiplication");
+      System.out.println("Grade 3: Division");
+      System.out.println("Grade 4: Word Problems");
+      System.out.println("Grade 5: Mix of Mathemtical Operations");
+      System.out.println("Grade 6: Decimals & Fractions");
+      System.out.println("Grade 7: Percents");
+      System.out.println("Grade 8: Exponents");
+      System.out.println("Grade 9: Complex Equations");
+      System.out.println("Grade 10: Probability");
+      System.out.println("Grade 11: Statistics");
+      System.out.println("Grade 12: Derivatives\n");
+
+      System.out.println("Select Grade Level (1-12): 1");
+      System.out.println("Select Difficulty (1-Easy 2-Medium 3-Hard): 1");
+      System.out.println("Select Game Mode (T-TimeBased P-ProblemBased): T\n");
+      
+      System.out.println("You have 60 seconds to complete as many problems as you can!");
+      System.out.println("Good Luck! Press Enter to start!\n");
+      
+      System.out.println("1 + 1 = 2");
+      System.out.println("Correct!\n");
+      System.out.println("1 + 5 = 5");
+      System.out.println("Incorrect!");
+   }
+}
